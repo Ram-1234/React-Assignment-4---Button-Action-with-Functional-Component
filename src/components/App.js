e8f3a1df85be3cd10a809ce69bd23f840fbfadda
Ram-1234 <ramnayan699@gmail.com>
2021-03-27 18:49:46 +0530

    modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-
 function App() {
+  const[state,setState]=useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+       {state ? (
+       <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+       ):(
+       <button id="click" onClick={()=> setState(true)}>
+      Click me
+    </button>
+    )
+    }
     </div>
   );
 }
-
-
 export default App;
